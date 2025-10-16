ddd06d67661ccba7d63a18e6f63c3e7e24b4b3ea
Hiruna-SP <hirunasp@gmail.com>
2025-10-16 11:47:36 +0530

    Add the share meal function

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,36 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+async function Meals(){
+  const meals = await getMeals();
+
+  return <MealGrid meals={meals}/>;
+}
 
 export default function MealsPage() {
+
+
   return (
-    <div>
-      <h1>Meals Page</h1>
-        <p>Welcome to the Meals page!</p>
-        <p><Link href="/">Go back to Home</Link></p>
-        <p><Link href="/meals/share">share</Link></p>
-        <p><Link href="/meals/meal">meal</Link></p>
-    </div>
+    <>
+    <header className={classes.header}>
+      <h1>
+        Delicious meals, created {''} <span className={classes.highlight}>by you</span>
+      </h1>
+    </header>
+    <p>
+      Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.
+    </p>
+    <p className={classes.cta}> 
+      <Link href="/meals/share">Share your meal</Link>
+    </p>
+    <main className={classes.main}>
+      <Suspense fallback={<p className={classes.loading}>Loading meals...</p>}>
+      <Meals />
+      </Suspense>
+    </main>
+    </>
   );
 }
---
+++ components/meals/image-picker.js
@@ -0,0 +1,58 @@
+"use client";
+
+import { use, useRef, useState } from "react";
+import classes from "./image-picker.module.css";
+import Image from "next/image";
+
+export default function ImagePicker({ label, name }) {
+  const imageInput = useRef();
+  const [pickedImage, setPickedImage] = useState();
+
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+        setPickedImage(null);
+      return;
+    }
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+  return (
+    <div className={classes.imagePicker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && <Image src={pickedImage} alt="Picked Image" fill />}
+        </div>
+
+        <input
+          className={classes.input}
+          style={{ display: "none" }}
+          type="file"
+          id={name}
+          name={name}
+          accept="image/png, image/jpeg"
+          ref={imageInput}
+          onChange={handleImageChange}
+          required
+        />
+
+        <button
+          type="button"
+          className={classes.button}
+          onClick={handlePickClick}
+        >
+          Pick Image
+        </button>
+      </div>
+    </div>
+  );
+}
